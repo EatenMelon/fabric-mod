76b6966d4cda300a4613a701ce37a85865de90e4
Michel Michels <michel@miche.ls>
2024-02-28 16:22:09 +0100

    Fix FoodComponent

--- src/eatenmelon/src/main/java/com/eatenmelon/items/Cross.java
+++ src/eatenmelon/src/main/java/com/eatenmelon/items/Cross.java
@@ -4,18 +4,25 @@ import com.mojang.datafixers.util.Pair;
 import net.minecraft.entity.effect.InstantStatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
+import net.minecraft.item.FoodComponents;
 import net.minecraft.item.Item;
 
 import java.util.ArrayList;
 
 public class Cross extends Item {
+    public static final FoodComponent foodComponent = new FoodComponent.Builder()
+            .hunger(6)
+            .saturationModifier(1)
+            .meat()
+            .alwaysEdible()
+            .snack()
+            .statusEffect(new StatusEffectInstance(StatusEffects.POISON, 6*20, 1), 1.0f)
+            .build();
 
     public Cross(Settings settings) {
-        ArrayList<Pair<StatusEffectInstance, Float>> statusEffects = new ArrayList<Pair<StatusEffectInstance, Float>>();
-        statusEffects.add(new Pair<StatusEffectInstance,Float>(new StatusEffectInstance(new InstantStatusEffect(StatusEffectCategory.HARMFUL,1),100)));
-        settings.food(new FoodComponent(6, 1,1,1,1,statusEffects))
-        .super(settings);
+        super(settings.food(foodComponent));
     }
 
 }
